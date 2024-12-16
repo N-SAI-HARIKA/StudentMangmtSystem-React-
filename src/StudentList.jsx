48a5ccf6d7943b943fef8e994ffc753749a1a8a8
Sai Harika Nanduri <118583056+N-SAI-HARIKA@users.noreply.github.com>
2024-12-16 09:13:04 +0530

    Update StudentList.jsx

--- src/StudentList.jsx
+++ src/StudentList.jsx
@@ -7,7 +7,7 @@ const StudentList = () => {
     const [searchTerm, setSearchTerm] = useState(""); // state for the search term
 
     useEffect(() => {
-        fetch("/db.json/students")
+        fetch("/db.json")
             .then((response) => {
                 if (!response.ok) {
                     throw new Error(`HTTP error! status: ${response.status}`);
